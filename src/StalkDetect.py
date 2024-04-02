e1e876793176108d128724f7059d445e883a3141
t-detlefsen <ted32@pitt.edu>
2024-04-02 17:33:30 -0400

    initial setup

---
+++ src/StalkDetect.py
@@ -0,0 +1,84 @@
+import yaml
+import rospy
+import rospkg
+
+from sensor_msgs.msg import CameraInfo
+from nimo_perception.srv import *
+
+class StalkDetect:
+    def __init__(self):
+        # Load config
+        self.loadConfig()
+
+        if self.verbose: rospy.loginfo('Starting nimo_perception node.')
+
+        # Check camera connection
+        try:
+            rospy.wait_for_message(self.camera_info_topic, CameraInfo, timeout=5)
+        except rospy.ROSException:
+            rospy.logwarn('Camera info not found, so camera is likely not running!')
+
+        # Initialize variables
+
+        # Setup services
+        rospy.Service('GetStalks', GetStalks, self.getStalks)
+        rospy.Service('GetWidth', GetWidth, self.getWidth)
+
+        if self.verbose: rospy.loginfo('Waiting for service calls...')
+
+    def loadConfig(self):
+        '''
+        Load configuration from yaml file
+        '''
+
+        rospack = rospkg.RosPack()
+        config_path = rospack.get_path('nimo_perception') + '/config/default.yaml'
+        with open(config_path) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+
+        self.verbose = config["debug"]["verbose"]
+        self.visualize = config["debug"]["visualize"]
+        self.save_images = config["debug"]["save_images"]
+
+        self.camera_info_topic = config["camera"]["info_topic"]
+
+    def getStalks(self, req: GetStalksRequest) -> GetStalksResponse:
+        '''
+        Determine the suitable stalks within view
+
+        Parameters
+            req (GetStalkRequest): The request: 
+                                   - num_frames - The number of frames to process
+                                   - timeout - the timeout in seconds to wait until aborting
+
+        Returns
+            GetStalkResponse: The response:
+                              - success - The success of the operation (SUCCESS / REPOSITION / ERROR)
+                              - num_frames - The number of frames processed
+                              - grasp_points - A list of grasp points on the cornstalks ordered from best to worst
+        '''
+
+        if self.verbose: rospy.loginfo('Received a GetStalks request for {} frames with timeout {} seconds'.format(req.num_frames, req.timeout))
+
+    def getWidth(self, req: GetWidthRequest) -> GetWidthResponse:
+        '''
+        Determine the width of a cornstalk
+
+        Parameters
+            req (GetStalkRequest): The request: 
+                                   - num_frames - The number of frames to process
+                                   - timeout - the timeout in seconds to wait until aborting
+
+        Returns
+            GetStalkResponse: The response:
+                              - success - The success of the operation (SUCCESS / REPOSITION / ERROR)
+                              - num_frames - The number of frames processed
+                              - width - The width of the cornstalk in mm
+        '''
+
+        if self.verbose: rospy.loginfo('Received a GetWidths request for {} frames with timeout {} seconds'.format(req.num_frames, req.timeout))
+
+if __name__ == "__main__":
+    rospy.init_node('nimo_perception')
+    stalk_detect = StalkDetect()
+    rospy.spin()
