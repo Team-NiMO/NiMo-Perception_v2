f2393234df2675574bb47af6844fed041cf4b499
t-detlefsen <ted32@pitt.edu>
2024-04-11 17:30:58 -0400

    single frame stalk detection w/ visualization

--- src/StalkDetect.py
+++ src/StalkDetect.py
@@ -30,6 +30,7 @@ class StalkDetect:
 
         # Initialize variables
         self.cv_bridge = CvBridge()
+        self.visualizer = visualize.Visualizer(self.world_frame)
 
         if self.model_arch == "Mask_RCNN":
             self.model = Mask_RCNN.Mask_RCNN(self.model_path, self.model_threshold, self.model_device)
@@ -61,7 +62,6 @@ class StalkDetect:
         self.camera_info_topic = config["camera"]["info_topic"]
         self.camera_image_topic = config["camera"]["image_topic"]
         self.camera_depth_topic = config["camera"]["depth_topic"]
-        self.camera_frame = config["camera"]["camera_frame"]
         self.world_frame = config["camera"]["world_frame"]
 
         self.model_arch = config["model"]["model"]
@@ -69,7 +69,33 @@ class StalkDetect:
         self.model_threshold = config["model"]["score_threshold"]
         self.model_device = config["model"]["device"]
 
+        self.cluster_threshold = config["stalk"]["cluster_threshold"]
+
         self.config = config
+
+    def clusterStalks(self, stalks):
+        '''
+        Cluster the stalks from multiple frames
+
+        Parameters
+            stalks: The list of unclustered stalks from multiple frames
+
+        Returns
+            clustered_stalks: The list of clustered stalks
+        '''
+
+        # clustering_labels = [0]
+        # for position in positions[1:]:
+        #     min_distance = float('inf')
+        #     min_cluster = 0
+        #     for i in range(len(clustering_labels)):
+        #         if distance(position, positions[i]) < min_distance:
+        #             min_distance = distance(position, positions[i])
+        #             min_cluster = clustering_labels[i]
+
+        #     clustering_labels.append(min_cluster if min_distance < THRESHOLD else max(clustering_labels) + 1)
+
+        # clustering_labels = np.array(clustering_labels)
 
     def getStalksCallback(self, image, depth_image):
         '''
@@ -80,31 +106,34 @@ class StalkDetect:
             depth_image: The current depth frame
         '''
 
+        # Reset new stalks
+        new_stalks = []
+
         # Convert image messaages to arrays
         depth_image = np.array(self.cv_bridge.imgmsg_to_cv2(depth_image, desired_encoding="passthrough"), dtype=np.float32)
         image = self.cv_bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
 
         # Run detection and get feature points
         masks, output, scores = self.model.forward(image)
-        # stalks_features = self.getStalkFeatures(masks, depth_image)
 
         # Create stalk objects and add to running list
         for mask, score in zip(masks, scores):
             new_stalk = stalk.Stalk(mask, score, depth_image, self.camera_intrinsic, self.config)
 
+            # Append to list if stalks are valid
             if new_stalk.valid:
-                self.stalks.append(new_stalk)
+                new_stalks.append(new_stalks)
         
-        # VISUALIZE (stalk line, grasp point, features)
-        if self.visualize:
-            pass
-                
         if self.save_images:
             features_image = self.model.visualize(image, output)
-            for x, y, _ in new_stalk.cam_features:
-                cv2.circle(features_image, (int(x), int(y)), 2, (255, 255, 255), -1)
+            for new_stalk in new_stalks:
+                for x, y, _ in new_stalk.cam_features:
+                    cv2.circle(features_image, (int(x), int(y)), 2, (255, 255, 255), -1)
 
             cv2.imwrite(self.package_path+"/output/FEATURES{}-{}.png".format(self.inference_index, self.image_index), features_image)
+
+        for new_stalk in new_stalks:
+            self.stalks.append(new_stalk)
 
         self.image_index += 1
 
@@ -147,7 +176,7 @@ class StalkDetect:
         self.stalks = []
         self.image_index = 0
         try:
-            self.inference_index = max([int(f[6:].split("-")[0]) for f in os.listdir(self.package_path+"/output")]) + 1
+            self.inference_index = max([int(f[len("FEATURES"):].split("-")[0]) for f in os.listdir(self.package_path+"/output")]) + 1
         except:
             self.inference_index = 0
 
@@ -167,7 +196,16 @@ class StalkDetect:
         depth_susbscriber.unregister()
         del ts
 
+        if len(self.stalks) == 0:
+            rospy.logwarn('No valid stalks detected in any frame for this service request, requesting a REPOSITION')
+            return GetStalksResponse(success='REPOSITION', num_frames=self.image_index + 1)
+        
         # Cluster stalks + average
+
+        # # Visualize
+        # self.visualizer.clearMarkers()
+        # self.visualizer.publishStalk(new_stalk.world_features)
+        # self.visualizer.publishGraspPoint(new_stalk.grasp_point)
 
         # Return with list
 
---
+++ src/nimo_perception/utils/visualize.py
@@ -0,0 +1,107 @@
+import numpy as np
+import rospy
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+
+class Visualizer:
+    def __init__(self, marker_frame):
+        # Define templates for stalk line and grasp point
+        self.defineTemplates(marker_frame)
+
+        # Initialize publishers
+        self.stalk_publisher = rospy.Publisher("stalk_line", Marker, queue_size=10)
+        self.grasp_publisher = rospy.Publisher("grasp_point", Marker, queue_size=10)
+
+        self.id = 0
+
+    def clearMarkers(self):
+        '''
+        Clear all of the markers currently displayed
+        '''
+        self.stalk_publisher.publish(self.remove_markers)
+        self.grasp_publisher.publish(self.remove_markers)
+        self.id = 0
+
+    def defineTemplates(self, marker_frame):
+        '''
+        Define the templates for stalk line and grasp point
+
+        Parameters
+            marker_frame: The frame to which the markers should be published
+        '''
+
+        # Stalk marker
+        self.stalk_marker = Marker()
+        self.stalk_marker.header.frame_id = marker_frame
+        self.stalk_marker.ns = 'points_to_line'
+        self.stalk_marker.type = Marker.LINE_STRIP
+        self.stalk_marker.action = Marker.ADD
+        self.stalk_marker.pose.orientation.w = 1.0
+        self.stalk_marker.scale.x = 0.01
+        self.stalk_marker.color.g = 1.0
+        self.stalk_marker.color.a = 1.0
+
+        # Grasp marker
+        self.grasp_marker = Marker()
+        self.grasp_marker.header.frame_id = marker_frame
+        self.grasp_marker.type = 2
+        self.grasp_marker.pose.orientation.w = 1.0
+        self.grasp_marker.scale.x = 0.015
+        self.grasp_marker.scale.y = 0.015
+        self.grasp_marker.scale.z = 0.015
+        self.grasp_marker.color.r = 1.0
+        self.grasp_marker.color.g = 0.08
+        self.grasp_marker.color.b = 0.6
+        self.grasp_marker.color.a = 1.0
+
+        # Clear markers
+        self.remove_markers = Marker()
+        self.remove_markers.id = 0
+        self.remove_markers.action = Marker.DELETEALL
+
+    def publishStalk(self, stalk_features):
+        '''
+        Create a stalk line marker based on stalk features
+
+        Parameters
+            stalk_features: The stalk features to turn into a line
+
+        Returns
+            marker: The line marker
+        '''
+        
+        heights = np.array([z for _, _, z in stalk_features])
+        min_p = stalk_features[np.argmin(heights)]
+        max_p = stalk_features[np.argmax(heights)]
+
+        bottom_point = Point(x=min_p[0], y=min_p[1], z=min_p[2])
+        top_point = Point(x=max_p[0], y=max_p[1], z=max_p[2])
+
+        marker = self.stalk_marker
+        marker.header.stamp = rospy.Time.now()
+        marker.id = self.id
+        marker.points = [top_point, bottom_point]
+
+        self.id += 1
+        
+        self.stalk_publisher.publish(marker)
+
+    def publishGraspPoint(self, grasp_point):
+        '''
+        Create a grasp point marker based on the grasp point
+
+        Parameters
+            grasp_point: The grasp point to turn into a marker
+
+        Returns
+            marker: The grasp point marker
+        '''
+
+        x, y, z = grasp_point
+        marker = self.grasp_marker
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = z
+        marker.id = self.id
+
+        self.grasp_publisher.publish(marker)
